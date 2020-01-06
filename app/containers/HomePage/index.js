b1f4c101c275ca8faa4cdd265fbfc36ac577dd7e
t3db0t <t3db0t@gmail.com>
2020-01-05 22:07:43 -0500

    clean

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,13 +6,11 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
 export default function HomePage() {
   return (
     <h1>
-      <FormattedMessage {...messages.header} />
+      Offline-test
     </h1>
   );
 }
